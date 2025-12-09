3ec7742ce8780508527aeefe6bf86c2083a03219
Ammar2408 <ammarsiddiqui078690@gmail.com>
2025-12-09 23:29:04 +0530

    added

--- intro.js
+++ intro.js
@@ -1,1 +1,12 @@
-console.log("Welcome to the Intro.js file!");
+console.log("Hi, my name is Ammar Ahmad.");
+console.log("I am starting my journey into JavaScript and Git/GitHub.");
+console.log("Today I learned:");
+console.log("- How to install Node.js");
+console.log("- How to run JavaScript files using Node");
+console.log("- How to create a GitHub repository");
+console.log("- How to push code from local machine to GitHub");
+console.log("");
+console.log("My first JavaScript program is running successfully!");
+console.log("⏱ Current Date & Time:", new Date().toLocaleString());
+console.log("");
+console.log("Excited to learn more JavaScript & build real projects soon!");
